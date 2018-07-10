82e438c371d00c73f95aa8e04bbbead75beb3c2c
sashayakovtseva <sashayakovtseva@gmail.com>
2018-07-10 15:55:37 +0300

    dfs is rolled back if cycle was found, test added

--- dfs.go
+++ dfs.go
@@ -5,15 +5,22 @@ import (
 )
 
 type (
-	edge struct {
-		to int
-		w  float32
+	// Arrow represents a single arrow in a directed graph.
+	Arrow struct {
+		// To holds id of a vertext which arrow goes to.
+		To int
+		// W is a weight of an arrow.
+		W float32
 	}
 
-	graph map[int][]edge
+	// Arrows is a set of arrows from the particular vertex.
+	Arrows []Arrow
+
+	// Graph is graph.
+	Graph map[int]Arrows
 )
 
-func dfs(g graph) {
+func dfs(g Graph) {
 	enter := make(map[int]int)     // holds ts of vertex entering
 	left := make(map[int]int)      // holds ts of vertex leaving
 	lifo := make([]int, 0, len(g)) // lifo to hold vertexes in progress
@@ -41,38 +48,51 @@ func dfs(g graph) {
 			// for each linked vertex
 			for i := last[cur]; i < len(g[cur]); i++ {
 				e := g[cur][i]
+				last[cur] = i + 1
 
-				// if linked vertex was not visited yet
-				if enter[e.to] == 0 {
+				// if linked vertex is not visited yet
+				if enter[e.To] == 0 {
 					// add next vertex to the lifo and continue
 					ts++
-					enter[e.to] = ts
-					lifo = append(lifo, e.to)
-					last[cur] = i + 1
+					enter[e.To] = ts
+					lifo = append(lifo, e.To)
 
-					fmt.Printf("found tree edge from %d to %d with cost %.2f\n", cur, e.to, e.w)
-					fmt.Printf("entered %d at %d\n", e.to, enter[e.to])
+					fmt.Printf("found tree edge from %d to %d with cost %.2f\n", cur, e.To, e.W)
+					fmt.Printf("entered %d at %d\n", e.To, enter[e.To])
 					continue deep
 				}
 
-				// found vertex is not in the lifo!
-				if left[e.to] == 0 {
-					// if both vertexes are in the lifo
+				if left[e.To] == 0 {
+					// if both vertices are in the lifo
 					// we have found the back edge, aka cycle
-					fmt.Printf("found back edge from %d to %d with cost %.2f\n", cur, e.to, e.w)
-					fmt.Printf("cycle is %v\n", lifo)
-				} else if enter[cur] < enter[e.to] {
+					fmt.Printf("found back edge from %d to %d with cost %.2f\n", cur, e.To, e.W)
+					cycle := extractCycle(e.To, lifo) // find the cycle vertices
+					relaxCycle(g, cycle)              // modify graph
+					// clear data for vertices in the cycle as if we never actualy visited them
+					for _, c := range cycle {
+						last[c] = 0
+						enter[c] = 0
+					}
+					// pop lifo to restart dfs
+					lifo = lifo[:len(lifo)-len(cycle)]
+					if len(lifo) != 0 {
+						last[lifo[len(lifo)-1]] = 0
+					}
+
+					fmt.Printf("restarting dfs on modified graph\n")
+					continue deep
+				} else if enter[cur] < enter[e.To] {
 					// if the linked vertex was entered after the current vertex
 					// we have found the forward edge
-					fmt.Printf("found forward edge from %d to %d with cost %.2f\n", cur, e.to, e.w)
+					fmt.Printf("found forward edge from %d to %d with cost %.2f\n", cur, e.To, e.W)
 				} else {
 					// if the linked vertex was entered before the current vertex
 					// we have found the cross edge
-					fmt.Printf("found cross edge from %d to %d with cost %.2f\n", cur, e.to, e.w)
+					fmt.Printf("found cross edge from %d to %d with cost %.2f\n", cur, e.To, e.W)
 				}
 			}
 
-			// we have visited all linked vertexes by now
+			// we have visited all linked vertices by now
 			// leave the current too
 			ts++
 			left[cur] = ts
@@ -80,4 +100,52 @@ func dfs(g graph) {
 			fmt.Printf("left %d at %d\n", cur, left[cur])
 		}
 	}
+}
+
+func extractCycle(to int, lifo []int) []int {
+	var cycle []int
+	for i := len(lifo) - 1; i >= 0; i-- {
+		if lifo[i] == to {
+			cycle = append(cycle, lifo[i:]...)
+			break
+		}
+	}
+	fmt.Printf("cycle is %v\n", cycle)
+	return cycle
+}
+
+func relaxCycle(g Graph, cycle []int) {
+	var min float32
+
+	// find minimum cost edge in the cycle
+	for i := 0; i < len(cycle); i++ {
+		cur := cycle[i]
+		for _, e := range g[cur] {
+			if e.To == cycle[(i+1)%len(cycle)] {
+				if e.W < min || min == 0 {
+					min = e.W
+				}
+				break
+			}
+		}
+	}
+
+	fmt.Printf("cycle throughput is %.2f\n", min)
+	fmt.Printf("%+v\n", g)
+
+	// relax all cycle edges according to found cost
+	for i := 0; i < len(cycle); i++ {
+		cur := cycle[i]
+		for j := range g[cur] {
+			if g[cur][j].To == cycle[(i+1)%len(cycle)] {
+				g[cur][j].W -= min
+				if g[cur][j].W == 0 {
+					g[cur] = append(g[cur][:j], g[cur][j+1:]...)
+				}
+				break
+			}
+		}
+	}
+
+	fmt.Printf("%+v\n", g)
 }
--- dfs_test.go
+++ dfs_test.go
@@ -1,22 +1,79 @@
 package relax
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestDFS(t *testing.T) {
-	g := graph{
-		1: []edge{
-			{to: 2, w: 3},
+	g := Graph{
+		1: Arrows{
+			{To: 2, W: 3},
 		},
-		2: []edge{
-			{to: 4, w: 1},
+		2: Arrows{
+			{To: 4, W: 1},
 		},
-		3: []edge{
-			{to: 1, w: 5},
-			{to: 2, w: 2},
+		3: Arrows{
+			{To: 1, W: 5},
+			{To: 2, W: 2},
 		},
-		4: []edge{
-			{to: 1, w: 4},
+		4: Arrows{
+			{To: 1, W: 4},
+		},
+	}
+	expect := Graph{
+		1: Arrows{
+			{To: 2, W: 2},
+		},
+		2: Arrows{},
+		3: Arrows{
+			{To: 1, W: 5},
+			{To: 2, W: 2},
+		},
+		4: Arrows{
+			{To: 1, W: 3},
 		},
 	}
 	dfs(g)
+	require.Equal(t, expect, g)
+}
+func TestDFS2(t *testing.T) {
+	g := Graph{
+		1: Arrows{
+			{To: 2, W: 3},
+		},
+		2: Arrows{
+			{To: 4, W: 2},
+		},
+		3: Arrows{
+			{To: 2, W: 7},
+		},
+		4: Arrows{
+			{To: 3, W: 1},
+			{To: 1, W: 5},
+		},
+		5: Arrows{
+			{To: 4, W: 9},
+			{To: 1, W: 6},
+		},
+	}
+	expect := Graph{
+		1: Arrows{
+			{To: 2, W: 2},
+		},
+		2: Arrows{},
+		3: Arrows{
+			{To: 2, W: 6},
+		},
+		4: Arrows{
+			{To: 1, W: 4},
+		},
+		5: Arrows{
+			{To: 4, W: 9},
+			{To: 1, W: 6},
+		},
+	}
+	dfs(g)
+	require.Equal(t, expect, g)
 }
